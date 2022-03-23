d702e9cd5f41516e915020fa4e479aa90b350647
nmcanall <nmcanall@citadel.edu>
2022-03-22 20:11:11 -0500

    setup local storage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
+// import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+    const [notes, setNotes] = React.useState(() => JSON.parse(localStorage.getItem("note")) || [])
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
+    )
+
+    React.useEffect(() => 
+      localStorage.setItem("note", JSON.stringify(notes)),
+      [notes]
     )
     
     function createNewNote() {
